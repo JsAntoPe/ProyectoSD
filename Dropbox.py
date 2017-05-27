05702298204c78ebb16ec0055197603630bbd92a
JsAntoPe <jsperegrina1@gmail.com>
2017-05-27 10:16:06 +0200

    Intentando implementar las colas para poder transferir los ficheros

--- Dropbox.py
+++ Dropbox.py
@@ -1,13 +1,32 @@
 import dropbox
 import pendulum
+import pika
+import time
 
 token = "kvbe4Epe2OAAAAAAAAAACFLOXRE34frCMWlvINIBHhfGehOAifIhED4gxvvVfhyU"
 dbx = dropbox.Dropbox(token)
 
-def subida(file):
+connection1 = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel1 = connection1.channel()
+
+channel1.queue_declare(queue='fileTrans', durable=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    time.sleep(body.count(b'.'))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+channel1.basic_qos(prefetch_count=1)
+channel1.basic_consume(callback,
+                      queue='fileTrans')
+
+def subida():
     now = pendulum.now('Europe/Madrid')
 
     #En este hueco debe ir la toma de datos, para pasarla a la variable.
+    channel1.start_consuming()
     with open(file, "rb") as f:
         data = f.read()
     #
--- Productor.py
+++ Productor.py
@@ -1,15 +1,10 @@
-from pyasn1_modules.rfc2315 import data
 import pika
 import sys
-#import extraccion
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='fileTrans', durable=True)
-
-"""for i in range(0, 5):
-    result = extraccion.ext.delay()"""
 
 #Modifcar esto para realizar la transferencia de archivos
 message = ' '.join(sys.argv[1:]) or "Hello World!"
