154cd7c8da28f75112b48e4ddf6192b9886415af
alvarodg <alvaroduran.96@gmail.com>
2017-05-29 03:52:29 +0200

    Delete dropboxSubidaProcesada.py

--- tutorial/tutorial/spiders/dropboxSubidaProcesada.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-import Dropbox
-
-app4 = Celery('dropboxSubidaProcesada', broker="pyamqp://guest@localhost//")
-
-
-@app4.task(no_ack=True)
-def dropbox_subida_procesado(data):
-    Dropbox.subidaProcesada(data)
