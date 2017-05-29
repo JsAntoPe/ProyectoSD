b963a25748b28a128c1bfd909869eabbdebaa947
alvarodg <alvaroduran.96@gmail.com>
2017-05-29 13:11:33 +0200

    Explicacion tareas periodicas

--- tutorial/tutorial/spiders/stackperiodico.py
+++ tutorial/tutorial/spiders/stackperiodico.py
@@ -27,9 +27,11 @@ class StackSpider(scrapy.Spider):
 
 app = Celery(backend="rpc://")
 
-
+# Configuración de tareas periódicas
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
+	# Parámetros de sender.add_periodic_class: 
+	# (periodo, nombre_funcion.s(parametro), name=nombre_cualquiera)
 	sender.add_periodic_task(10.0, empezar_crawl.s(), name='scrape every 10')
 	"""sender.add_periodic_task(
 		crontab(hour=7, minute=30, day_of_week=1),
@@ -38,8 +40,10 @@ def setup_periodic_tasks(sender, **kwargs):
 
 
 app.conf.timezone = 'Europe/London'
+# Necesario para que no se intente reiniciar el twisted reactor
 app.conf.worker_max_tasks_per_child = 1
 
+# Tarea que se va a ejecutar
 @app.task(name = "procesa")
 def empezar_crawl():
 	print("funcionando")
